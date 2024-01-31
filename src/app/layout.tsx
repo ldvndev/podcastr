cb272a4a79febc44c118a62e0fb13603d137ac11
ldvndev <ldvndev@gmail.com>
2024-01-31 19:49:30 -0300

    feat: Adding Global Styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import '../styles/global.scss'
 
 export const metadata: Metadata = {
   title: "Podcastr",
@@ -15,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
